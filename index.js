842c5a9716c8a3ac6f2cf84a8f02d96411473853
kalyanijha <kalyanijha20.02.2008@gmail.com>
2025-07-29 17:08:17 +0530

    Added root route to redirect to /posts

--- index.js
+++ index.js
@@ -1,97 +1,95 @@
-const { log } = require("console");
 const express = require("express");
-const app = express();
-// const port = 8080;
-const port = process.env.PORT || 8080;
-
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const { v4: uuidv4 } = require("uuid");
 const methodOverride = require("method-override");
 
-app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'));
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// View engine setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
+// Dummy data
 let posts = [
-    {
-        id: uuidv4(),
-        username: "kalyani_jha",
-        content: "i love coading!!"
-    },
-    {
-        id: uuidv4(),
-        username: "sharadhakhapra",
-        content: "hardwork is imp to achieve something  gr8!"
-    },
-    {
-        id: uuidv4(),
-        username: "apnaCllg",
-        content: "I got selected in ISRO!"
-    },
-]
+  {
+    id: uuidv4(),
+    username: "kalyani_jha",
+    content: "i love coding!!",
+  },
+  {
+    id: uuidv4(),
+    username: "sharadhakhapra",
+    content: "hardwork is important to achieve something great!",
+  },
+  {
+    id: uuidv4(),
+    username: "apnaCllg",
+    content: "I got selected in ISRO!",
+  },
+];
 
-// see all post
+// Routes
+app.get("/", (req, res) => {
+    res.redirect("/posts");
+});
+
+
+// Show all posts
 app.get("/posts", (req, res) => {
-    res.render("index.ejs", { posts });
+  res.render("index.ejs", { posts });
 });
 
-// create routes
-// 1. to create form
-app.get("/posts/new", (req, res) =>{
-    res.render("new.ejs");
-});
-// 2. to add form in index.ejs
-app.post("/posts", (req,res) =>{
-    let { username, content } = req.body;
-    let id = uuidv4();
-    posts.push({ id, username, content});
-    res.redirect("/posts");
+// Form to create new post
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
 });
 
-// specific post view
-app.get("/posts/:id", (req, res) =>{
-    let { id } = req.params;
-    let post = posts.find((post) => id === post.id);
-    console.log(post);
-    res.render("show.ejs",{ post });
+// Create new post
+app.post("/posts", (req, res) => {
+  const { username, content } = req.body;
+  const id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
 });
 
-// patch rqst
-app.patch("/posts/:id", (req, res) => { 
-    let { id } = req.params;
-    console.log(id);
-    let newContent = req.body.content;
-    let post = posts.find((p) => id === p.id);
-    post.content = newContent;
-
-    console.log(post);
-    res.redirect("/posts");
+// Show specific post
+app.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((post) => post.id === id);
+  res.render("show.ejs", { post });
 });
 
-
-// Edit
-app.get("/posts/:id/edit", (req, res) =>{
-    let { id } = req.params;
-    console.log(id);
-    let post = posts.find((post)=> id === post.id);
-    res.render("edit.ejs", {post});
-
+// Edit form
+app.get("/posts/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((post) => post.id === id);
+  res.render("edit.ejs", { post });
 });
 
-// delete
-app.delete("/posts/:id",(req, res) =>{
-    let { id } = req.params;
-    posts = posts.filter((p) => id !== p.id);
-    res.redirect("/posts");
-} )
+// Update post
+app.patch("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const post = posts.find((p) => p.id === id);
+  post.content = content;
+  res.redirect("/posts");
+});
 
+// Delete post
+app.delete("/posts/:id", (req, res) => {
+  const { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  res.redirect("/posts");
+});
 
-app.listen(port, ()=>{
-    console.log("listening on port: 8080");
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
